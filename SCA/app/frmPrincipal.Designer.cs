f620ce9abbead47ee175417de7c1f0620a48c0aa
f-Rra <facundo.rra@gmail.com>
2025-09-16 23:26:03 -0300

    Correcciones en frmPrincipal

--- SCA/app/frmPrincipal.Designer.cs
+++ SCA/app/frmPrincipal.Designer.cs
@@ -33,10 +33,12 @@ namespace app
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.poisonComboBox1 = new ReaLTaiizor.Controls.PoisonComboBox();
+            this.panel6 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.button1 = new ReaLTaiizor.Controls.Button();
-            this.comboBoxEdit1 = new ReaLTaiizor.Controls.ComboBoxEdit();
+            this.panel7 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new ReaLTaiizor.Controls.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
@@ -58,37 +60,81 @@ namespace app
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(243)))), ((int)(((byte)(157)))));
             this.panel2.Location = new System.Drawing.Point(12, 132);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(103, 10);
+            this.panel2.Size = new System.Drawing.Size(103, 6);
             this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.panel6);
             this.panel3.Controls.Add(this.pictureBox1);
             this.panel3.Controls.Add(this.panel2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel3.Location = new System.Drawing.Point(0, 25);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(127, 695);
+            this.panel3.Size = new System.Drawing.Size(137, 695);
             this.panel3.TabIndex = 2;
             // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.panel7);
+            this.panel4.Controls.Add(this.poisonComboBox1);
             this.panel4.Controls.Add(this.pictureBox2);
             this.panel4.Controls.Add(this.button1);
-            this.panel4.Controls.Add(this.comboBoxEdit1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(127, 25);
+            this.panel4.Location = new System.Drawing.Point(137, 25);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1153, 142);
+            this.panel4.Size = new System.Drawing.Size(1143, 142);
             this.panel4.TabIndex = 3;
+            // 
+            // poisonComboBox1
+            // 
+            this.poisonComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(225)))));
+            this.poisonComboBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.poisonComboBox1.FontSize = ReaLTaiizor.Extension.Poison.PoisonComboBoxSize.Tall;
+            this.poisonComboBox1.FormattingEnabled = true;
+            this.poisonComboBox1.ItemHeight = 29;
+            this.poisonComboBox1.Items.AddRange(new object[] {
+            "Comedor",
+            "Quincho"});
+            this.poisonComboBox1.Location = new System.Drawing.Point(55, 56);
+            this.poisonComboBox1.Name = "poisonComboBox1";
+            this.poisonComboBox1.Size = new System.Drawing.Size(121, 35);
+            this.poisonComboBox1.TabIndex = 0;
+            this.poisonComboBox1.UseSelectable = true;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(34)))), ((int)(((byte)(33)))));
+            this.panel6.Location = new System.Drawing.Point(127, 6);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(6, 683);
+            this.panel6.TabIndex = 0;
             // 
             // panel5
             // 
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(127, 167);
+            this.panel5.Location = new System.Drawing.Point(137, 167);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1153, 553);
+            this.panel5.Size = new System.Drawing.Size(1143, 553);
             this.panel5.TabIndex = 4;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(243)))), ((int)(((byte)(157)))));
+            this.panel7.Location = new System.Drawing.Point(6, 132);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(1125, 6);
+            this.panel7.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::app.Properties.Resources.lugar;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(27, 35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
             // 
             // button1
             // 
@@ -98,56 +144,25 @@ namespace app
             this.button1.EnteredBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
             this.button1.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
             this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = null;
+            this.button1.Image = global::app.Properties.Resources.iniciar;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
-            this.button1.Location = new System.Drawing.Point(208, 45);
+            this.button1.Location = new System.Drawing.Point(192, 50);
             this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
             this.button1.PressedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
             this.button1.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.button1.Size = new System.Drawing.Size(142, 46);
+            this.button1.Size = new System.Drawing.Size(153, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Iniciar Servicio";
             this.button1.TextAlignment = System.Drawing.StringAlignment.Center;
             // 
-            // comboBoxEdit1
-            // 
-            this.comboBoxEdit1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(225)))));
-            this.comboBoxEdit1.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.comboBoxEdit1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.comboBoxEdit1.DropDownHeight = 100;
-            this.comboBoxEdit1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxEdit1.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.comboBoxEdit1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(34)))), ((int)(((byte)(33)))));
-            this.comboBoxEdit1.FormattingEnabled = true;
-            this.comboBoxEdit1.HoverSelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(225)))));
-            this.comboBoxEdit1.IntegralHeight = false;
-            this.comboBoxEdit1.ItemHeight = 20;
-            this.comboBoxEdit1.Items.AddRange(new object[] {
-            "Comedor",
-            "Quincho"});
-            this.comboBoxEdit1.Location = new System.Drawing.Point(55, 56);
-            this.comboBoxEdit1.Name = "comboBoxEdit1";
-            this.comboBoxEdit1.Size = new System.Drawing.Size(132, 26);
-            this.comboBoxEdit1.StartIndex = 0;
-            this.comboBoxEdit1.TabIndex = 1;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::app.Properties.Resources.lugar;
-            this.pictureBox2.Location = new System.Drawing.Point(28, 56);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(21, 26);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::app.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 6);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(103, 120);
+            this.pictureBox1.Size = new System.Drawing.Size(112, 120);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -179,10 +194,12 @@ namespace app
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.PictureBox pictureBox1;
         private ReaLTaiizor.Controls.Button button1;
-        private ReaLTaiizor.Controls.ComboBoxEdit comboBoxEdit1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private ReaLTaiizor.Controls.PoisonComboBox poisonComboBox1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
     }
 }
