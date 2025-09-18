1612a55dd821cf2705cb744c3f35c2656a565356
f-Rra <facundo.rra@gmail.com>
2025-09-18 20:28:43 -0300

    Sidebar completo

--- SCA/app/frmPrincipal.cs
+++ SCA/app/frmPrincipal.cs
@@ -24,24 +24,58 @@ namespace app
             InitializeComponent();
         }
 
-        private void pbxHome_Click(object sender, EventArgs e)
+        private void btnHome_Click(object sender, EventArgs e)
         {
-            ssSidebar.Location = new System.Drawing.Point(23, 169);
+            ssSidebar.Location = new System.Drawing.Point(9, 169);
         }
 
-        private void pbxRegistro_Click(object sender, EventArgs e)
+        private void btnRegistros_Click(object sender, EventArgs e)
         {
-            ssSidebar.Location = new System.Drawing.Point(23, 237);
+            ssSidebar.Location = new System.Drawing.Point(9, 251);
         }
 
-        private void pbxReporte_Click(object sender, EventArgs e)
+        private void btnReportes_Click(object sender, EventArgs e)
         {
-            ssSidebar.Location = new System.Drawing.Point(23, 307);
+            ssSidebar.Location = new System.Drawing.Point(9, 332);
         }
 
-        private void pbxAdmin_Click(object sender, EventArgs e)
+        private void btnAdmin_Click(object sender, EventArgs e)
         {
-            ssSidebar.Location = new System.Drawing.Point(23, 567);
+            ssSidebar.Location = new System.Drawing.Point(9, 537);
+        }
+
+   
+        private readonly Color MenuBase = Color.FromArgb(35, 34, 33);
+        private readonly Color MenuHover = Color.FromArgb(243, 229, 201);
+
+        private void AplicarHover(Panel contenedor, Label etiqueta, bool hover)
+        {
+            if (contenedor == null || etiqueta == null) return;
+            contenedor.BackColor = hover ? MenuHover : Color.Transparent;
+            etiqueta.ForeColor = hover ? Color.Black : Color.Transparent;
+            etiqueta.Cursor = Cursors.Hand;
+        }
+
+        private void Menu_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is Label lblSender && lblSender.Parent is Panel p1)
+                AplicarHover(p1, lblSender, true);
+            else if (sender is Panel p2)
+            {
+                var lblChild = p2.Controls.OfType<Label>().FirstOrDefault();
+                if (lblChild != null) AplicarHover(p2, lblChild, true);
+            }
+        }
+
+        private void Menu_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Label lblSender && lblSender.Parent is Panel p1)
+                AplicarHover(p1, lblSender, false);
+            else if (sender is Panel p2)
+            {
+                var lblChild = p2.Controls.OfType<Label>().FirstOrDefault();
+                if (lblChild != null) AplicarHover(p2, lblChild, false);
+            }
         }
     }
 }
