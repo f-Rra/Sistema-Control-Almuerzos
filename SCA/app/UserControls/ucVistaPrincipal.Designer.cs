05139176ef7b06ab53a40a1eb3d1ab41fa0adfca
f-Rra <facundo.rra@gmail.com>
2025-09-21 18:45:18 -0300

    Correcciones en controles

--- SCA/app/UserControls/ucVistaPrincipal.Designer.cs
+++ SCA/app/UserControls/ucVistaPrincipal.Designer.cs
@@ -28,32 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            this.registrosBinding = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.registrosBinding)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvRegistros
             // 
-            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRegistros.Location = new System.Drawing.Point(61, 54);
+            this.dgvRegistros.AllowUserToAddRows = false;
+            this.dgvRegistros.AllowUserToDeleteRows = false;
+            this.dgvRegistros.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvRegistros.AutoGenerateColumns = false;
+            this.dgvRegistros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvRegistros.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(225)))));
+            this.dgvRegistros.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvRegistros.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(34)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(34)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegistros.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvRegistros.ColumnHeadersHeight = 28;
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvRegistros.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dgvRegistros.DataSource = this.registrosBinding;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(225)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(208)))), ((int)(((byte)(36)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvRegistros.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvRegistros.EnableHeadersVisualStyles = false;
+            this.dgvRegistros.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(34)))), ((int)(((byte)(33)))));
+            this.dgvRegistros.Location = new System.Drawing.Point(24, 24);
             this.dgvRegistros.Name = "dgvRegistros";
-            this.dgvRegistros.Size = new System.Drawing.Size(1036, 400);
+            this.dgvRegistros.ReadOnly = true;
+            this.dgvRegistros.RowHeadersVisible = false;
+            this.dgvRegistros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRegistros.Size = new System.Drawing.Size(1107, 462);
             this.dgvRegistros.TabIndex = 0;
             // 
             // ucVistaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(225)))));
             this.Controls.Add(this.dgvRegistros);
             this.Name = "ucVistaPrincipal";
             this.Size = new System.Drawing.Size(1155, 510);
             ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.registrosBinding)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
         private System.Windows.Forms.DataGridView dgvRegistros;
+        private System.Windows.Forms.BindingSource registrosBinding;
     }
 }
--- SCA/app/UserControls/ucVistaPrincipal.cs
+++ SCA/app/UserControls/ucVistaPrincipal.cs
@@ -14,60 +14,12 @@ namespace app.UserControls
 {
     public partial class ucVistaPrincipal : UserControl
     {
-    private readonly BindingSource registrosBinding = new BindingSource();
     private readonly RegistroNegocio negR = new RegistroNegocio();
     private int? servicioIdActual = null;
 
         public ucVistaPrincipal()
         {
             InitializeComponent();
-            ConfigurarGrilla();
-        }
-
-        private void ConfigurarGrilla()
-        {
-            if (dgvRegistros == null) return;
-            var colorCrema = Color.FromArgb(255, 248, 225);     // #FFF8E1
-            var colorNegro = Color.FromArgb(35, 34, 33);         // #232221
-            var colorDoradoOsc = Color.FromArgb(255, 208, 36);   // #ffd024
-            var colorDoradoClaro = Color.FromArgb(253, 243, 157);// #fdf39d
-            var colorHover = Color.FromArgb(243, 229, 201);      // similar a MenuHover
-
-            this.BackColor = colorCrema;
-
-            dgvRegistros.AutoGenerateColumns = true;
-            dgvRegistros.ReadOnly = true;
-            dgvRegistros.AllowUserToAddRows = false;
-            dgvRegistros.AllowUserToDeleteRows = false;
-            dgvRegistros.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvRegistros.MultiSelect = false;
-            dgvRegistros.RowHeadersVisible = false;
-            dgvRegistros.DataSource = registrosBinding;
-
-            dgvRegistros.Dock = DockStyle.None;
-            dgvRegistros.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-
-            dgvRegistros.BackgroundColor = colorCrema;
-            dgvRegistros.BorderStyle = BorderStyle.None;
-            dgvRegistros.GridColor = colorNegro;
-            dgvRegistros.EnableHeadersVisualStyles = false;
-            dgvRegistros.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-
-            dgvRegistros.ColumnHeadersDefaultCellStyle.BackColor = colorNegro;
-            dgvRegistros.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-            dgvRegistros.ColumnHeadersDefaultCellStyle.SelectionBackColor = colorNegro;
-            dgvRegistros.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.White;
-            dgvRegistros.ColumnHeadersHeight = 28;
-            dgvRegistros.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10.5f, FontStyle.Bold);
-            dgvRegistros.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-
-            dgvRegistros.DefaultCellStyle.BackColor = colorCrema;
-            dgvRegistros.DefaultCellStyle.ForeColor = Color.Black;
-            dgvRegistros.DefaultCellStyle.SelectionBackColor = colorDoradoOsc;
-            dgvRegistros.DefaultCellStyle.SelectionForeColor = Color.Black;
-            dgvRegistros.DefaultCellStyle.Font = new Font("Segoe UI", 10f, FontStyle.Regular);
-            dgvRegistros.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dgvRegistros.AlternatingRowsDefaultCellStyle.BackColor = colorHover;
         }
 
         private void BindRegistros(IList<Registro> registros)
