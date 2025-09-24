2bc09d1a4289d8aabfb0c6186a9438a69ee4a795
f-Rra <facundo.rra@gmail.com>
2025-09-24 18:32:54 -0300

    Nuevos UCs agregados + Funcionalidad sidebar

---
+++ SCA/app/UserControls/ucRegistroManual.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app.UserControls
+{
+    public partial class ucRegistroManual : UserControl
+    {
+        private int? servicioIdActual;
+        private int idLugarActual;
+
+        public ucRegistroManual()
+        {
+            InitializeComponent();
+        }
+
+        // Recibe el contexto del servicio activo y el lugar seleccionado
+        public void SetServicio(int servicioId, int idLugar)
+        {
+            servicioIdActual = servicioId;
+            idLugarActual = idLugar;
+
+            // TODO: si este UC necesita cargar datos en base al servicio/lugar,
+            // hacelo acá (e.g., CargarEmpleadosSinAlmorzar(servicioIdActual.Value, idLugarActual)).
+        }
+    }
+}
---
+++ SCA/app/UserControls/ucReportes.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace app.UserControls
+{
+    partial class ucReportes
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+            this.SuspendLayout();
+            // 
+            // bigLabel1
+            // 
+            this.bigLabel1.AutoSize = true;
+            this.bigLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.bigLabel1.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.bigLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.bigLabel1.Location = new System.Drawing.Point(435, 198);
+            this.bigLabel1.Name = "bigLabel1";
+            this.bigLabel1.Size = new System.Drawing.Size(153, 46);
+            this.bigLabel1.TabIndex = 0;
+            this.bigLabel1.Text = "Reportes";
+            // 
+            // ucReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.bigLabel1);
+            this.Name = "ucReportes";
+            this.Size = new System.Drawing.Size(1155, 510);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Controls.BigLabel bigLabel1;
+    }
+}
