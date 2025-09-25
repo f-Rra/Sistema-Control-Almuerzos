14d9acba83523065b7bea2e1a6b0ee94bd7d0a30
f-Rra <facundo.rra@gmail.com>
2025-09-25 19:41:53 -0300

    Funcionalidad en filtros de busqueda

--- SCA/negocio/EmpleadoNegocio.cs
+++ SCA/negocio/EmpleadoNegocio.cs
@@ -191,5 +191,66 @@ namespace Negocio
                 datos.cerrarConexion();
             }
         }
+
+        // Método unificado para filtrar empleados sin almorzar
+        public List<Empleado> filtrarEmpleadosSinAlmorzar(int idServicio, int? idEmpresa = null, string nombre = null)
+        {
+            List<Empleado> lista = new List<Empleado>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearProcedimiento("SP_FiltrarEmpleadosSinAlmorzar");
+                datos.setearParametro("@IdServicio", idServicio);
+                
+                // Parámetros opcionales
+                if (idEmpresa.HasValue)
+                    datos.setearParametro("@IdEmpresa", idEmpresa.Value);
+                else
+                    datos.setearParametro("@IdEmpresa", DBNull.Value);
+                    
+                if (!string.IsNullOrWhiteSpace(nombre))
+                    datos.setearParametro("@Nombre", nombre);
+                else
+                    datos.setearParametro("@Nombre", DBNull.Value);
+                
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Empleado empleado = new Empleado();
+                    empleado.IdEmpleado = (int)datos.Lector["IdEmpleado"];
+                    empleado.Nombre = (string)datos.Lector["Nombre"];
+                    empleado.Apellido = (string)datos.Lector["Apellido"];
+                    empleado.IdCredencial = (string)datos.Lector["IdCredencial"];
+                    empleado.IdEmpresa = (int)datos.Lector["IdEmpresa"];
+                    empleado.NombreEmpresa = (string)datos.Lector["Empresa"];
+
+                    lista.Add(empleado);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        // Empleados sin almorzar filtrados por empresa (usa método unificado)
+        public List<Empleado> empleadosSinAlmorzarPorEmpresa(int idServicio, int idEmpresa)
+        {
+            return filtrarEmpleadosSinAlmorzar(idServicio, idEmpresa, null);
+        }
+
+        // Empleados sin almorzar filtrados por nombre (usa método unificado)
+        public List<Empleado> empleadosSinAlmorzarPorNombre(int idServicio, string nombre)
+        {
+            return filtrarEmpleadosSinAlmorzar(idServicio, null, nombre);
+        }
     }
 }
