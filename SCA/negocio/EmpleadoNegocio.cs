3c4741a7402599b8256293b64e35d3a269e01b48
f-Rra <facundo.rra@gmail.com>
2025-10-04 20:36:39 -0300

    Procedimientos y controles para ucAdmin

--- SCA/negocio/EmpleadoNegocio.cs
+++ SCA/negocio/EmpleadoNegocio.cs
@@ -11,38 +11,43 @@ namespace Negocio
     {
         public List<Empleado> listar()
         {
-            List<Empleado> lista = new List<Empleado>();
-            AccesoDatos datos = new AccesoDatos();
-
-            try
+            return ExceptionHelper.EjecutarConManejo(() =>
             {
-                datos.setearConsulta("SP_ListarEmpleados");
-                datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
-                datos.ejecutarLectura();
+                List<Empleado> lista = new List<Empleado>();
+                AccesoDatos datos = new AccesoDatos();
 
-                while (datos.Lector.Read())
+                try
                 {
-                    Empleado empleado = new Empleado();
-                    empleado.IdEmpleado = (int)datos.Lector["IdEmpleado"];
-                    empleado.Nombre = (string)datos.Lector["Nombre"];
-                    empleado.Apellido = (string)datos.Lector["Apellido"];
-                    empleado.IdCredencial = (string)datos.Lector["IdCredencial"];
-                    empleado.IdEmpresa = (int)datos.Lector["IdEmpresa"];
-                    empleado.NombreEmpresa = (string)datos.Lector["Empresa"];
+                    datos.setearConsulta("SP_ListarTodosLosEmpleados");
+                    datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
+                    datos.ejecutarLectura();
 
-                    lista.Add(empleado);
+                    while (datos.Lector.Read())
+                    {
+                        Empleado empleado = new Empleado();
+                        empleado.IdEmpleado = (int)datos.Lector["IdEmpleado"];
+                        empleado.Nombre = (string)datos.Lector["Nombre"];
+                        empleado.Apellido = (string)datos.Lector["Apellido"];
+                        empleado.IdCredencial = (string)datos.Lector["IdCredencial"];
+                        empleado.IdEmpresa = (int)datos.Lector["IdEmpresa"];
+                        empleado.NombreEmpresa = (string)datos.Lector["Empresa"];
+                        empleado.Estado = (bool)datos.Lector["Estado"];
+
+                        // Crear objeto Empresa
+                        empleado.Empresa = new Empresa();
+                        empleado.Empresa.IdEmpresa = empleado.IdEmpresa;
+                        empleado.Empresa.Nombre = empleado.NombreEmpresa;
+
+                        lista.Add(empleado);
+                    }
+
+                    return lista;
                 }
-
-                return lista;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-            finally
-            {
-                datos.cerrarConexion();
-            }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }, "cargar empleados");
         }
 
         public Empleado buscarPorCredencial(string credencial)
@@ -196,6 +201,135 @@ namespace Negocio
             {
                 datos.cerrarConexion();
             }
+        }
+
+        public void agregar(Empleado empleado)
+        {
+            ExceptionHelper.EjecutarConManejo(() =>
+            {
+                AccesoDatos datos = new AccesoDatos();
+                try
+                {
+                    datos.setearConsulta("SP_AgregarEmpleado");
+                    datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
+                    datos.setearParametro("@IdCredencial", empleado.IdCredencial);
+                    datos.setearParametro("@Nombre", empleado.Nombre);
+                    datos.setearParametro("@Apellido", empleado.Apellido);
+                    datos.setearParametro("@IdEmpresa", empleado.Empresa.IdEmpresa);
+                    datos.setearParametro("@Estado", empleado.Estado);
+                    
+                    datos.ejecutarAccion();
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }, "agregar empleado");
+        }
+
+        public void modificar(Empleado empleado)
+        {
+            ExceptionHelper.EjecutarConManejo(() =>
+            {
+                AccesoDatos datos = new AccesoDatos();
+                try
+                {
+                    datos.setearConsulta("SP_ModificarEmpleado");
+                    datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
+                    datos.setearParametro("@IdEmpleado", empleado.IdEmpleado);
+                    datos.setearParametro("@IdCredencial", empleado.IdCredencial);
+                    datos.setearParametro("@Nombre", empleado.Nombre);
+                    datos.setearParametro("@Apellido", empleado.Apellido);
+                    datos.setearParametro("@IdEmpresa", empleado.Empresa.IdEmpresa);
+                    datos.setearParametro("@Estado", empleado.Estado);
+                    
+                    datos.ejecutarAccion();
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }, "modificar empleado");
+        }
+
+        public void eliminar(int idEmpleado)
+        {
+            ExceptionHelper.EjecutarConManejo(() =>
+            {
+                AccesoDatos datos = new AccesoDatos();
+                try
+                {
+                    datos.setearConsulta("SP_DesactivarEmpleado");
+                    datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
+                    datos.setearParametro("@IdEmpleado", idEmpleado);
+                    datos.ejecutarAccion();
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }, "desactivar empleado");
+        }
+
+        public bool existeCredencial(string credencial)
+        {
+            return ExceptionHelper.EjecutarConManejo(() =>
+            {
+                AccesoDatos datos = new AccesoDatos();
+                try
+                {
+                    datos.setearConsulta("SP_VerificarCredencialExiste");
+                    datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
+                    datos.setearParametro("@IdCredencial", credencial);
+                    datos.ejecutarLectura();
+                    
+                    if (datos.Lector.Read())
+                    {
+                        return (int)datos.Lector["Existe"] > 0;
+                    }
+                    return false;
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }, "verificar credencial");
+        }
+
+        public Empleado buscarPorId(int id)
+        {
+            return ExceptionHelper.EjecutarConManejo(() =>
+            {
+                AccesoDatos datos = new AccesoDatos();
+                Empleado empleado = new Empleado();
+                
+                try
+                {
+                    datos.setearConsulta("SP_BuscarEmpleadoPorId");
+                    datos.setearTipoComando(System.Data.CommandType.StoredProcedure);
+                    datos.setearParametro("@IdEmpleado", id);
+                    datos.ejecutarLectura();
+                    
+                    if (datos.Lector.Read())
+                    {
+                        empleado.IdEmpleado = (int)datos.Lector["IdEmpleado"];
+                        empleado.IdCredencial = datos.Lector["IdCredencial"].ToString();
+                        empleado.Nombre = datos.Lector["Nombre"].ToString();
+                        empleado.Apellido = datos.Lector["Apellido"].ToString();
+                        empleado.Estado = (bool)datos.Lector["Estado"];
+                        
+                        empleado.Empresa = new Empresa();
+                        empleado.Empresa.IdEmpresa = (int)datos.Lector["IdEmpresa"];
+                        empleado.Empresa.Nombre = datos.Lector["NombreEmpresa"].ToString();
+                    }
+                    
+                    return empleado;
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }, "buscar empleado por ID");
         }
 
     }
